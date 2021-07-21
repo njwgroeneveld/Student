5e0ebe43661a19d82afe428b7f68275aea26153a
niels <njwgroeneveld@gmail.com>
2021-07-21 14:55:51 +0200

    first commit, Student Class

---
+++ src/test/java/com/test2/TestSample2/TestSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test2.TestSample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
