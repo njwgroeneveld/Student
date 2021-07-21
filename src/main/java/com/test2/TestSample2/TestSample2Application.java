2596b135c7f74c42b7a4dd84910756636605690b
niels <njwgroeneveld@gmail.com>
2021-07-21 15:35:37 +0200

    first commit, Student Class

--- src/main/java/com/test2/TestSample2/TestSample2Application.java
+++ src/main/java/com/test2/TestSample2/TestSample2Application.java
@@ -1,33 +1,16 @@
 package com.test2.TestSample2;
 
-import com.test2.TestSample2.student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class TestSample2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSample2Application.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Mariam",
-						"jamal@gmail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21
-				)
-		);
-	}
+
 }
