3e1e6b42b68cfd72befffc62e8cebaeb534ae83f
niels <njwgroeneveld@gmail.com>
2021-07-22 10:36:58 +0200

    Added @Transient annotation for Age, field age gets calculated by date of birth.

--- src/main/java/com/test2/TestSample2/student/StudentConfig.java
+++ src/main/java/com/test2/TestSample2/student/StudentConfig.java
@@ -18,16 +18,14 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5),
-                    21
+                    LocalDate.of(2000, Month.JANUARY, 5)
+
             );
 
             Student alex= new Student(
                     "Alex",
                     "Alex@gmail.com",
-                    LocalDate.of(2004, Month.JANUARY, 5),
-                    21
-
+                    LocalDate.of(2004, Month.JANUARY, 5)
             );
 
             repository.saveAll(
